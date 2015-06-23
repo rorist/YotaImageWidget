fea5e104a3d05730eab91f051fc54997e0f58915
Rorist <rorist@0xcafe.ch>
2015-06-23 21:08:16 +0200

    rearange buttons

--- app/src/main/java/info/lamatricexiste/yotaimage/YotaImageConfig.java
+++ app/src/main/java/info/lamatricexiste/yotaimage/YotaImageConfig.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 
