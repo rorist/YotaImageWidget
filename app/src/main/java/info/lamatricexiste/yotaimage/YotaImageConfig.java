3f219fe52a74e3272723287d0bf04ce055c62c99
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2015-07-08 14:33:10 +0200

    Use GPLv3

--- app/src/main/java/info/lamatricexiste/yotaimage/YotaImageConfig.java
+++ app/src/main/java/info/lamatricexiste/yotaimage/YotaImageConfig.java
@@ -1,3 +1,22 @@
+/*
+
+  Copyright (C) 2015 Aubort Jean-Baptiste (Rorist)
+
+  This file is part of YotaImageWidget.
+
+  YotaImageWidget is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  YotaImageWidget is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with YotaImageWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package info.lamatricexiste.yotaimage;
 
 import android.app.Activity;
